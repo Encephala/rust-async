c654375367298a2a23c63b766dfd5ae4e53b5093
Encephala <jonathan_rietveld@hotmail.com>
2024-02-08 19:05:35 +0100

    Use actual workload to see CPU usage

--- src/bin/async_await/main.rs
+++ src/bin/async_await/main.rs
@@ -1,9 +1,8 @@
 // https://rust-lang.github.io/async-book/01_getting_started/04_async_await_primer.html
-use async_std::task::sleep;
 use futures::{executor::block_on, future::join_all};
 
-use std::time::Duration;
-use rand::Rng;
+const NUM_TASKS: usize = 200;
+const MAX_PARAM: usize = 10_000_000;
 
 fn expensive_calculation() {
     let _: Vec<_> = (0..MAX_PARAM).map(|n| f64::sqrt(n as f64)).collect();
@@ -16,29 +15,22 @@ struct Task {
 impl Task {
     // Some expensive calculation
     async fn execute(&self) -> usize {
-        sleep(Duration::from_millis(self.id as u64)).await;
-        println!("Finished task {}", self.id);
+        expensive_calculation();
+        println!("Executed task {}", self.id);
         return self.id;
     }
 }
-
-const NUM_TASKS: usize = 200;
-const MIN_WAIT_TIME_MILLIS: usize = 1;
-const MAX_WAIT_TIME_MILLIS: usize = 5000;
 
 async fn process_task(task: Task) -> usize {
     return task.execute().await;
 }
 
 async fn spawn_tasks() {
-    let mut rng = rand::thread_rng();
-
     // Spawn tasks, collect future
-    let task_futures: Vec<_> = (0..NUM_TASKS).map(|_| {
-        let duration = rng.gen_range(MIN_WAIT_TIME_MILLIS..MAX_WAIT_TIME_MILLIS);
+    let task_futures: Vec<_> = (0..NUM_TASKS).map(|i| {
+        println!("\tQueued task {i}");
 
-        println!("\tQueued task {duration}");
-        process_task(Task { id: duration })
+        process_task(Task { id: i })
     }).collect();
 
     // Execute all tasks
